0d87b78ebdfa0b0d38de66c132df959667aefb10
oldbanana <oldbanana@email.cz>
2024-01-20 20:02:07 +0100

    Simple test and Ctor DI+primary bean implemented

--- src/test/java/springframework/spring6DIdemo/controllers/CtorInjectedControllerTest.java
+++ src/test/java/springframework/spring6DIdemo/controllers/CtorInjectedControllerTest.java
@@ -3,8 +3,6 @@ package springframework.spring6DIdemo.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class CtorInjectedControllerTest {
     @Autowired//this annotation is mandatory, without it controller field would be null because its not instantiated
