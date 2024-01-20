56bf8292a202c52e60c7af5c8428da5f5c170ddb
oldbanana <oldbanana@email.cz>
2024-01-20 21:01:38 +0100

    Simple Qualifier example

--- src/main/java/springframework/spring6DIdemo/controllers/CtorInjectedController.java
+++ src/main/java/springframework/spring6DIdemo/controllers/CtorInjectedController.java
@@ -1,13 +1,15 @@
 package springframework.spring6DIdemo.controllers;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import springframework.spring6DIdemo.services.GreetingService;
 
 @Controller//it also means  to be a Spring managed component
 public class CtorInjectedController {
     private final GreetingService greetingService;
-
-    public CtorInjectedController(GreetingService greetingService) {
+    //now it will pickup the original instance instead of primary one, also for some reason first letter is lower,
+    //not capital: "greeting..."
+    public CtorInjectedController(@Qualifier("greetingServiceImpl") GreetingService greetingService) {
         this.greetingService = greetingService;
         //Spring needs to create an instance of this
         //so it will execute it and inject a greeting service into the ctor
--- src/main/java/springframework/spring6DIdemo/services/GreetingServiceImpl.java
+++ src/main/java/springframework/spring6DIdemo/services/GreetingServiceImpl.java
@@ -3,6 +3,8 @@ package springframework.spring6DIdemo.services;
 import org.springframework.stereotype.Service;
 
 @Service//it means  to be a Spring managed component
+//I could also make custom name to service via @Service("blablabla") and then in controller inside class use:
+//@Qualifier("blablabla") so it will prioritize this bean
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
